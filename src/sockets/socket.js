446657c77a8f76afbdf6231ba3cce6b15e31bb24
Darwin Gomez <darwintnt@hotmail.com>
2020-10-02 19:59:15 -0500

    first commit

--- src/sockets/socket.js
+++ src/sockets/socket.js
@@ -1,5 +1,16 @@
 const { io } = require('../server/server');
 
 io.on('connection', (client) => {
-  
+
+  console.log('Usuario conectado');
+
+  client.emit('sendMessage', {
+    usuario: 'Administrador',
+    mensaje: 'Bienvenido a esta aplicación'
+  });
+
+  client.on('disconnect', () => {
+    console.log('Usuario desconectado');
+  });
+
 });
